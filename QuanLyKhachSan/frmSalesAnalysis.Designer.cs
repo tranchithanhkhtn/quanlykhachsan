814d872b4faa9f80581d360bf3f75a34ae44bb21
tranchithanhkhtn <68425158+tranchithanhkhtn@users.noreply.github.com>
2021-01-08 20:22:17 +0700

    SalesAnalysis

---
+++ QuanLyKhachSan/frmSalesAnalysis.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class frmSalesAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbDay = new System.Windows.Forms.Label();
+            this.lbMonth = new System.Windows.Forms.Label();
+            this.lbYear = new System.Windows.Forms.Label();
+            this.chartSalesAnalysis = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartSalesAnalysis)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Aqua;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1141, 5);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Tomato;
+            this.label2.Location = new System.Drawing.Point(15, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(327, 29);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "PHÂN TÍCH THUÊ PHÒNG";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Black Wolf", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label1.Location = new System.Drawing.Point(700, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 35;
+            this.label1.Text = "Day";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Black Wolf", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label3.Location = new System.Drawing.Point(823, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 35;
+            this.label3.Text = "Month";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Black Wolf", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.label4.Location = new System.Drawing.Point(957, 23);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(54, 20);
+            this.label4.TabIndex = 35;
+            this.label4.Text = "Year";
+            // 
+            // lbDay
+            // 
+            this.lbDay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbDay.AutoSize = true;
+            this.lbDay.Font = new System.Drawing.Font("Black Wolf", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbDay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(57)))), ((int)(((byte)(93)))));
+            this.lbDay.Location = new System.Drawing.Point(763, 23);
+            this.lbDay.Name = "lbDay";
+            this.lbDay.Size = new System.Drawing.Size(31, 20);
+            this.lbDay.TabIndex = 35;
+            this.lbDay.Text = "dd";
+            // 
+            // lbMonth
+            // 
+            this.lbMonth.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbMonth.AutoSize = true;
+            this.lbMonth.Font = new System.Drawing.Font("Black Wolf", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMonth.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(57)))), ((int)(((byte)(93)))));
+            this.lbMonth.Location = new System.Drawing.Point(906, 23);
+            this.lbMonth.Name = "lbMonth";
+            this.lbMonth.Size = new System.Drawing.Size(39, 20);
+            this.lbMonth.TabIndex = 35;
+            this.lbMonth.Text = "MM";
+            // 
+            // lbYear
+            // 
+            this.lbYear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbYear.AutoSize = true;
+            this.lbYear.Font = new System.Drawing.Font("Black Wolf", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbYear.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(57)))), ((int)(((byte)(93)))));
+            this.lbYear.Location = new System.Drawing.Point(1030, 23);
+            this.lbYear.Name = "lbYear";
+            this.lbYear.Size = new System.Drawing.Size(53, 20);
+            this.lbYear.TabIndex = 35;
+            this.lbYear.Text = "yyyy";
+            // 
+            // chartSalesAnalysis
+            // 
+            this.chartSalesAnalysis.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea1.Name = "ChartArea1";
+            this.chartSalesAnalysis.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartSalesAnalysis.Legends.Add(legend1);
+            this.chartSalesAnalysis.Location = new System.Drawing.Point(12, 66);
+            this.chartSalesAnalysis.Name = "chartSalesAnalysis";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.Legend = "Legend1";
+            series1.Name = "SL Khách Thuê";
+            this.chartSalesAnalysis.Series.Add(series1);
+            this.chartSalesAnalysis.Size = new System.Drawing.Size(1117, 571);
+            this.chartSalesAnalysis.TabIndex = 36;
+            this.chartSalesAnalysis.Text = "chart1";
+            // 
+            // frmSalesAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 649);
+            this.Controls.Add(this.chartSalesAnalysis);
+            this.Controls.Add(this.lbYear);
+            this.Controls.Add(this.lbMonth);
+            this.Controls.Add(this.lbDay);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmSalesAnalysis";
+            this.Text = "frmSalesAnalysis";
+            this.Load += new System.EventHandler(this.frmSalesAnalysis_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartSalesAnalysis)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbDay;
+        private System.Windows.Forms.Label lbMonth;
+        private System.Windows.Forms.Label lbYear;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartSalesAnalysis;
+    }
+}
---
+++ QuanLyKhachSan/frmSalesAnalysis.cs
@@ -0,0 +1,49 @@
+﻿using QuanLyKhachSan.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class frmSalesAnalysis : Form
+    {
+        public frmSalesAnalysis()
+        {
+            InitializeComponent();
+        }
+
+        void Load_Chart()
+        {
+            DataTable dt = SalesAnalysisDAO.Instance.Revenue_SalesAnalysis();
+
+            for (int j = 0; j < dt.Rows.Count; j++)
+            {
+                for (int i = 0; i < 24; i++)
+                {
+                    if (Int32.Parse(dt.Rows[j]["GioThue"].ToString().Substring(0, 2)) == i)
+                    {
+                        this.chartSalesAnalysis.Series["SL Khách Thuê"].Points.AddXY(Int32.Parse(dt.Rows[j]["GioThue"].ToString().Substring(0, 2)), dt.Rows[j]["Số Lượt Thuê"]);
+                    }
+                    else
+                    {
+                        this.chartSalesAnalysis.Series["SL Khách Thuê"].Points.AddXY(i, 0);
+                    }
+                }
+            }
+        }
+
+        private void frmSalesAnalysis_Load(object sender, EventArgs e)
+        {
+            lbDay.Text = DateTime.Now.ToString("dd");
+            lbMonth.Text = DateTime.Now.ToString("MM");
+            lbYear.Text = DateTime.Now.ToString("yyyy");
+            Load_Chart();
+        }
+    }
+}
