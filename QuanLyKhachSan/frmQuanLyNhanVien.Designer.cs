e64c9c45baca50280c26396a3bb6278094ca994f
tranchithanhkhtn <68425158+tranchithanhkhtn@users.noreply.github.com>
2021-01-08 20:19:14 +0700

    QuanLyNhanVien

---
+++ QuanLyKhachSan/frmQuanLyNhanVien.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class frmQuanLyNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmQuanLyNhanVien));
+            this.dtgvListOfEmployee = new System.Windows.Forms.DataGridView();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ptClose = new System.Windows.Forms.PictureBox();
+            this.MaNV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TenNV = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.GTinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NgSinh = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SDT = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DiaChi = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvListOfEmployee)).BeginInit();
+            this.contextMenuStrip.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgvListOfEmployee
+            // 
+            this.dtgvListOfEmployee.AllowUserToAddRows = false;
+            this.dtgvListOfEmployee.AllowUserToDeleteRows = false;
+            this.dtgvListOfEmployee.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dtgvListOfEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgvListOfEmployee.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.MaNV,
+            this.TenNV,
+            this.GTinh,
+            this.NgSinh,
+            this.SDT,
+            this.DiaChi});
+            this.dtgvListOfEmployee.ContextMenuStrip = this.contextMenuStrip;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(0, 4, 0, 4);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dtgvListOfEmployee.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dtgvListOfEmployee.Location = new System.Drawing.Point(-7, 43);
+            this.dtgvListOfEmployee.Name = "dtgvListOfEmployee";
+            this.dtgvListOfEmployee.ReadOnly = true;
+            this.dtgvListOfEmployee.RowTemplate.Height = 25;
+            this.dtgvListOfEmployee.Size = new System.Drawing.Size(1148, 676);
+            this.dtgvListOfEmployee.TabIndex = 0;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.viewToolStripMenuItem,
+            this.deleteToolStripMenuItem,
+            this.updateToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(132, 82);
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("viewToolStripMenuItem.Image")));
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("deleteToolStripMenuItem.Image")));
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            // 
+            // updateToolStripMenuItem
+            // 
+            this.updateToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("updateToolStripMenuItem.Image")));
+            this.updateToolStripMenuItem.Name = "updateToolStripMenuItem";
+            this.updateToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.updateToolStripMenuItem.Text = "Update";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1141, 42);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Danh Sách Nhân Viên";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // ptClose
+            // 
+            this.ptClose.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ptClose.Image = ((System.Drawing.Image)(resources.GetObject("ptClose.Image")));
+            this.ptClose.Location = new System.Drawing.Point(6, 7);
+            this.ptClose.Name = "ptClose";
+            this.ptClose.Size = new System.Drawing.Size(30, 30);
+            this.ptClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptClose.TabIndex = 32;
+            this.ptClose.TabStop = false;
+            this.ptClose.Click += new System.EventHandler(this.ptClose_Click);
+            // 
+            // MaNV
+            // 
+            this.MaNV.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.MaNV.DataPropertyName = "MaNV";
+            this.MaNV.FillWeight = 97.76797F;
+            this.MaNV.HeaderText = "Employee Code";
+            this.MaNV.Name = "MaNV";
+            this.MaNV.ReadOnly = true;
+            // 
+            // TenNV
+            // 
+            this.TenNV.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.TenNV.DataPropertyName = "TenNV";
+            this.TenNV.FillWeight = 133.6162F;
+            this.TenNV.HeaderText = "Full Name";
+            this.TenNV.Name = "TenNV";
+            this.TenNV.ReadOnly = true;
+            // 
+            // GTinh
+            // 
+            this.GTinh.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.GTinh.DataPropertyName = "GTinh";
+            this.GTinh.FillWeight = 51.54639F;
+            this.GTinh.HeaderText = "Sex";
+            this.GTinh.Name = "GTinh";
+            this.GTinh.ReadOnly = true;
+            // 
+            // NgSinh
+            // 
+            this.NgSinh.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.NgSinh.DataPropertyName = "NgSinh";
+            this.NgSinh.FillWeight = 83.45319F;
+            this.NgSinh.HeaderText = "Date of Birth";
+            this.NgSinh.Name = "NgSinh";
+            this.NgSinh.ReadOnly = true;
+            // 
+            // SDT
+            // 
+            this.SDT.DataPropertyName = "SDT";
+            this.SDT.HeaderText = "Number Phone";
+            this.SDT.Name = "SDT";
+            this.SDT.ReadOnly = true;
+            this.SDT.Width = 150;
+            // 
+            // DiaChi
+            // 
+            this.DiaChi.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.DiaChi.DataPropertyName = "DiaChi";
+            this.DiaChi.FillWeight = 133.6162F;
+            this.DiaChi.HeaderText = "Address";
+            this.DiaChi.Name = "DiaChi";
+            this.DiaChi.ReadOnly = true;
+            // 
+            // frmQuanLyNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 731);
+            this.Controls.Add(this.ptClose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtgvListOfEmployee);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmQuanLyNhanVien";
+            this.Text = "frmQuanLyNhanVien";
+            this.Load += new System.EventHandler(this.frmQuanLyNhanVien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgvListOfEmployee)).EndInit();
+            this.contextMenuStrip.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ptClose)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgvListOfEmployee;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox ptClose;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateToolStripMenuItem;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaNV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TenNV;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GTinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NgSinh;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SDT;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DiaChi;
+    }
+}
---
+++ QuanLyKhachSan/frmQuanLyNhanVien.cs
@@ -0,0 +1,49 @@
+﻿using QuanLyKhachSan.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyKhachSan
+{
+    public partial class frmQuanLyNhanVien : Form
+    {
+        public frmQuanLyNhanVien()
+        {
+            InitializeComponent();
+        }
+        string _email = "";
+        public frmQuanLyNhanVien(string email) : this()
+        {
+            _email = email;
+          
+        }
+        private void ptClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void frmQuanLyNhanVien_Load(object sender, EventArgs e)
+        {
+            dtgvListOfEmployee.AutoGenerateColumns = false;
+            dtgvListOfEmployee.DataSource = HomepageDAO.Instance.LoadListEmployee(_email);
+            dtgvListOfEmployee.BorderStyle = BorderStyle.None;
+            dtgvListOfEmployee.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(238, 239, 249);
+            dtgvListOfEmployee.CellBorderStyle = DataGridViewCellBorderStyle.SingleHorizontal;
+            dtgvListOfEmployee.DefaultCellStyle.SelectionBackColor = Color.DarkTurquoise;
+            dtgvListOfEmployee.DefaultCellStyle.SelectionForeColor = Color.WhiteSmoke;
+            dtgvListOfEmployee.BackgroundColor = Color.White;
+
+            dtgvListOfEmployee.EnableHeadersVisualStyles = false;
+            dtgvListOfEmployee.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dtgvListOfEmployee.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(20, 25, 72);
+            dtgvListOfEmployee.ColumnHeadersDefaultCellStyle.ForeColor = Color.White;
+
+        }
+    }
+}
