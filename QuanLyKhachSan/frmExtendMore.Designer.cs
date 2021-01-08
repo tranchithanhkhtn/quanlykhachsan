f6d29bc802e524a23f480586550e4662a67a8466
tranchithanhkhtn <68425158+tranchithanhkhtn@users.noreply.github.com>
2021-01-08 20:02:09 +0700

    ExtendMore.designer.cs

---
+++ QuanLyKhachSan/frmExtendMore.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class frmExtendMore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmExtendMore));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ptClose = new System.Windows.Forms.PictureBox();
+            this.guna2BorderlessForm1 = new Guna.UI2.WinForms.Guna2BorderlessForm(this.components);
+            this.btnRenewal = new Guna.UI2.WinForms.Guna2Button();
+            this.lbRentCode = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbCustomerName = new System.Windows.Forms.Label();
+            this.lbIdentityCard = new System.Windows.Forms.Label();
+            this.lbRoomCode = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbRenewalDate = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txbNumberDay = new System.Windows.Forms.TextBox();
+            this.plRoomCode = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.ptClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Aqua;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(751, 5);
+            this.panel1.TabIndex = 2;
+            // 
+            // ptClose
+            // 
+            this.ptClose.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ptClose.Image = ((System.Drawing.Image)(resources.GetObject("ptClose.Image")));
+            this.ptClose.Location = new System.Drawing.Point(709, 11);
+            this.ptClose.Name = "ptClose";
+            this.ptClose.Size = new System.Drawing.Size(30, 30);
+            this.ptClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptClose.TabIndex = 36;
+            this.ptClose.TabStop = false;
+            this.ptClose.Click += new System.EventHandler(this.ptClose_Click);
+            // 
+            // guna2BorderlessForm1
+            // 
+            this.guna2BorderlessForm1.BorderRadius = 10;
+            this.guna2BorderlessForm1.ContainerControl = this;
+            this.guna2BorderlessForm1.ShadowColor = System.Drawing.Color.Gray;
+            // 
+            // btnRenewal
+            // 
+            this.btnRenewal.AutoRoundedCorners = true;
+            this.btnRenewal.BackColor = System.Drawing.Color.Transparent;
+            this.btnRenewal.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnRenewal.BorderRadius = 22;
+            this.btnRenewal.BorderThickness = 2;
+            this.btnRenewal.CheckedState.Parent = this.btnRenewal;
+            this.btnRenewal.CustomImages.Parent = this.btnRenewal;
+            this.btnRenewal.FillColor = System.Drawing.Color.WhiteSmoke;
+            this.btnRenewal.Font = new System.Drawing.Font("VNI-Hobo", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRenewal.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnRenewal.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnRenewal.HoverState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnRenewal.HoverState.ForeColor = System.Drawing.Color.White;
+            this.btnRenewal.HoverState.Parent = this.btnRenewal;
+            this.btnRenewal.Location = new System.Drawing.Point(553, 258);
+            this.btnRenewal.Name = "btnRenewal";
+            this.btnRenewal.ShadowDecoration.BorderRadius = 26;
+            this.btnRenewal.ShadowDecoration.Color = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.btnRenewal.ShadowDecoration.Enabled = true;
+            this.btnRenewal.ShadowDecoration.Parent = this.btnRenewal;
+            this.btnRenewal.Size = new System.Drawing.Size(138, 47);
+            this.btnRenewal.TabIndex = 47;
+            this.btnRenewal.Text = "Gia Haïn";
+            this.btnRenewal.Click += new System.EventHandler(this.btnRenewal_Click);
+            // 
+            // lbRentCode
+            // 
+            this.lbRentCode.AutoSize = true;
+            this.lbRentCode.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRentCode.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lbRentCode.Location = new System.Drawing.Point(174, 15);
+            this.lbRentCode.Name = "lbRentCode";
+            this.lbRentCode.Size = new System.Drawing.Size(53, 23);
+            this.lbRentCode.TabIndex = 49;
+            this.lbRentCode.Text = "NULL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("VNI-Souvir", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Tomato;
+            this.label6.Location = new System.Drawing.Point(6, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 25);
+            this.label6.TabIndex = 48;
+            this.label6.Text = "Maõ Phieáu Thueâ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("VNI-Souvir", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Tomato;
+            this.label9.Location = new System.Drawing.Point(12, 69);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(124, 25);
+            this.label9.TabIndex = 50;
+            this.label9.Text = "Hoï Teân KH :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("VNI-Souvir", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Tomato;
+            this.label10.Location = new System.Drawing.Point(27, 124);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(109, 25);
+            this.label10.TabIndex = 51;
+            this.label10.Text = "Soá CMND :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("VNI-Souvir", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Tomato;
+            this.label1.Location = new System.Drawing.Point(439, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 25);
+            this.label1.TabIndex = 52;
+            this.label1.Text = "Maõ Phoøng :";
+            // 
+            // lbCustomerName
+            // 
+            this.lbCustomerName.AutoSize = true;
+            this.lbCustomerName.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCustomerName.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lbCustomerName.Location = new System.Drawing.Point(156, 71);
+            this.lbCustomerName.Name = "lbCustomerName";
+            this.lbCustomerName.Size = new System.Drawing.Size(53, 23);
+            this.lbCustomerName.TabIndex = 53;
+            this.lbCustomerName.Text = "NULL";
+            // 
+            // lbIdentityCard
+            // 
+            this.lbIdentityCard.AutoSize = true;
+            this.lbIdentityCard.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIdentityCard.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lbIdentityCard.Location = new System.Drawing.Point(156, 126);
+            this.lbIdentityCard.Name = "lbIdentityCard";
+            this.lbIdentityCard.Size = new System.Drawing.Size(53, 23);
+            this.lbIdentityCard.TabIndex = 54;
+            this.lbIdentityCard.Text = "NULL";
+            // 
+            // lbRoomCode
+            // 
+            this.lbRoomCode.AutoSize = true;
+            this.lbRoomCode.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRoomCode.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lbRoomCode.Location = new System.Drawing.Point(575, 71);
+            this.lbRoomCode.Name = "lbRoomCode";
+            this.lbRoomCode.Size = new System.Drawing.Size(53, 23);
+            this.lbRoomCode.TabIndex = 55;
+            this.lbRoomCode.Text = "NULL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("VNI-Souvir", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Tomato;
+            this.label2.Location = new System.Drawing.Point(439, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 25);
+            this.label2.TabIndex = 56;
+            this.label2.Text = "Ngaøy GH :";
+            // 
+            // lbRenewalDate
+            // 
+            this.lbRenewalDate.AutoSize = true;
+            this.lbRenewalDate.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbRenewalDate.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lbRenewalDate.Location = new System.Drawing.Point(563, 126);
+            this.lbRenewalDate.Name = "lbRenewalDate";
+            this.lbRenewalDate.Size = new System.Drawing.Size(53, 23);
+            this.lbRenewalDate.TabIndex = 57;
+            this.lbRenewalDate.Text = "NULL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("VNI-Souvir", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Tomato;
+            this.label3.Location = new System.Drawing.Point(123, 197);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(229, 25);
+            this.label3.TabIndex = 58;
+            this.label3.Text = "Soá Ngaøy Gia Haïn Theâm :";
+            // 
+            // txbNumberDay
+            // 
+            this.txbNumberDay.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.txbNumberDay.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbNumberDay.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbNumberDay.ForeColor = System.Drawing.Color.Silver;
+            this.txbNumberDay.Location = new System.Drawing.Point(392, 197);
+            this.txbNumberDay.Name = "txbNumberDay";
+            this.txbNumberDay.Size = new System.Drawing.Size(140, 21);
+            this.txbNumberDay.TabIndex = 59;
+            this.txbNumberDay.Text = "Enter Number";
+            this.txbNumberDay.TextChanged += new System.EventHandler(this.txbNumberDay_TextChanged);
+            this.txbNumberDay.Enter += new System.EventHandler(this.txbCodeRoom_Enter);
+            this.txbNumberDay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txbCodeRoom_KeyPress);
+            this.txbNumberDay.Leave += new System.EventHandler(this.txbCodeRoom_Leave);
+            // 
+            // plRoomCode
+            // 
+            this.plRoomCode.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.plRoomCode.Location = new System.Drawing.Point(392, 224);
+            this.plRoomCode.Name = "plRoomCode";
+            this.plRoomCode.Size = new System.Drawing.Size(140, 3);
+            this.plRoomCode.TabIndex = 60;
+            // 
+            // frmExtendMore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.ClientSize = new System.Drawing.Size(751, 329);
+            this.Controls.Add(this.txbNumberDay);
+            this.Controls.Add(this.plRoomCode);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbRenewalDate);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbCustomerName);
+            this.Controls.Add(this.lbIdentityCard);
+            this.Controls.Add(this.lbRoomCode);
+            this.Controls.Add(this.lbRentCode);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnRenewal);
+            this.Controls.Add(this.ptClose);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmExtendMore";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmExtendMore";
+            this.Load += new System.EventHandler(this.frmExtendMore_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.ptClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox ptClose;
+        private Guna.UI2.WinForms.Guna2BorderlessForm guna2BorderlessForm1;
+        private Guna.UI2.WinForms.Guna2Button btnRenewal;
+        private System.Windows.Forms.Label lbRentCode;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbRenewalDate;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbCustomerName;
+        private System.Windows.Forms.Label lbIdentityCard;
+        private System.Windows.Forms.Label lbRoomCode;
+        private System.Windows.Forms.TextBox txbNumberDay;
+        private System.Windows.Forms.Panel plRoomCode;
+    }
+}
