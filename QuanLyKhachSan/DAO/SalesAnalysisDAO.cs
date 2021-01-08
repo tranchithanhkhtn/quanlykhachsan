5025c2a8fa517b186fe6f094509c247063262cef
tranchithanhkhtn <68425158+tranchithanhkhtn@users.noreply.github.com>
2021-01-08 19:48:24 +0700

    SalesAnalysic

---
+++ QuanLyKhachSan/DAO/SalesAnalysisDAO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyKhachSan.DAO
+{
+    public class SalesAnalysisDAO
+    {
+        private static SalesAnalysisDAO instance;
+        public static SalesAnalysisDAO Instance
+        {
+            get { if (instance == null) instance = new SalesAnalysisDAO(); return SalesAnalysisDAO.instance; }
+            private set { SalesAnalysisDAO.instance = value; }
+        }
+        private SalesAnalysisDAO() { }
+        public DataTable Revenue_SalesAnalysis()
+        {
+            string sql = "select SUBSTRING(pt.GioThue, 1, 2) as [GioThue], count(*) AS [Số Lượt Thuê] from PHIEUTHUE pt join CT_PHIEUTHUE ctpt on pt.MaPT = ctpt.MaPT where pt.NgayThue = '" + DateTime.Now.ToString("yyyy-MM-dd") + "' group by SUBSTRING(pt.GioThue, 1, 2)";
+            DataTable data = DataProvider.Instance.ExecuteQuery(sql);
+            return data;
+        }
+
+    }
+}
